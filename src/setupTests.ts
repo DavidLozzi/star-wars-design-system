1572bb7b606d65f903ade59b099b08a289d044d3
David Lozzi <david@lozzi.net>
2025-07-21 07:15:54 -0400

    Tests

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,13 +1,32 @@
 import '@testing-library/jest-dom';
-
-declare global {
-  const jest: any;
-}
+import React from 'react';
 
 // Mock styled-components
+const createStyledComponent = (element: any) => {
+  return (strings: any, ...args: any[]) => {
+    const Component = (props: any) => {
+      const { children, ...restProps } = props;
+      return React.createElement(element, restProps, children);
+    };
+    Component.displayName = `Styled${element.charAt(0).toUpperCase() + element.slice(1)}`;
+    return Component;
+  };
+};
+
+const styled = new Proxy({}, {
+  get(target, prop) {
+    return createStyledComponent(prop);
+  }
+});
+
 jest.mock('styled-components', () => ({
-  ...jest.requireActual('styled-components'),
-  createGlobalStyle: jest.fn(() => 'div'),
+  __esModule: true,
+  default: styled,
+  ThemeProvider: (props: any) => {
+    const { children, ...restProps } = props;
+    return React.createElement('div', restProps, children);
+  },
+  keyframes: jest.fn(() => 'keyframes'),
 }));
 
 // Mock FontAwesome
